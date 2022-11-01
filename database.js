7673dcdfb8f8591f7e68a9f34fdee9d7dfc6d917
Kyle <klaflash@purdue.edu>
2022-11-01 19:40:59 -0400

    Moved location of event listeners

--- database.js
+++ database.js
@@ -29,8 +29,41 @@ router.post('/', (req, res) => {
     const { username, password } = req.body;
     if (username && password) {
         try {
-            db.promise().query(`INSERT INTO USERS VALUES('${username}', '${password}')`);
-            res.status(201).send({msg: 'Created User'});
+            //db.promise().query(`INSERT INTO USERS VALUES('${username}', '${password}')`);
+            //res.status(201).send({msg: 'Created User'});
+            document.getElementById('create-account').addEventListener(() => {
+                const username = document.getElementById('username').textContent;
+                const email = document.getElementById('email').textContent;
+                const pass = document.getElementById('pass').textContent;
+                const phone = document.getElementById('phone').textContent;
+                const balance = 100;
+            
+                try {
+                    db.promise().query(`INSERT INTO USER VALUES('${username}', '${pass}', '${email}', '${phone}', '${balance}')`);
+                } catch (err) {
+                    console.log(err);
+                }
+            });
+
+            document.getElementById('place').addEventListener(() => {
+
+                const bet_id = 0001;
+                const amount = document.getElementById('amount').textContent;
+                const description = 'Warriors vs. Heat';
+                const time = '7:30';
+                const payout = 2 * amount;
+                const username = 'TestUser';
+                const game_id = '0001';
+                const placed_odds = '+100';
+            
+                try {
+                    db.promise().query(`INSERT INTO BETS VALUES('${bet_id}', '${amount}', '${description}', '${time}', '${payout}', '${username}', '${game_id}', '${placed_odds}')`);
+                } catch (err) {
+                    console.log(err);
+                }
+            });
+
+
         } catch (err) {
             console.log(err);
         }
@@ -38,41 +71,11 @@ router.post('/', (req, res) => {
     }
 });
 
-document.getElementById('create-account').addEventListener(() => {
-    const username = document.getElementById('username').textContent;
-    const email = document.getElementById('email').textContent;
-    const pass = document.getElementById('pass').textContent;
-    const phone = document.getElementById('phone').textContent;
-    const balance = 100;
-
-    try {
-        db.promise().query(`INSERT INTO USER VALUES('${username}', '${pass}', '${email}', '${phone}', '${balance}')`);
-    } catch (err) {
-        console.log(err);
-    }
-});
-
 document.getElementById('login').addEventListener(() => {
 
 });
 
-document.getElementById('place').addEventListener(() => {
 
-    const bet_id = 0001;
-    const amount = document.getElementById('amount').textContent;
-    const description = 'Warriors vs. Heat';
-    const time = '7:30';
-    const payout = 2 * amount;
-    const username = 'TestUser';
-    const game_id = '0001';
-    const placed_odds = '+100';
-
-    try {
-        db.promise().query(`INSERT INTO BETS VALUES('${bet_id}', '${amount}', '${description}', '${time}', '${payout}', '${username}', '${game_id}', '${placed_odds}')`);
-    } catch (err) {
-        console.log(err);
-    }
-});
 
 
 
