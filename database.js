b95bd0c8d311497f67ddb03778ff55f0ee66b99e
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2022-12-05 17:46:00 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,83 +1,40 @@
-const { Router } = require('express');
-const mysql = require('mysql');
+let db = openDatabase("itemDB", "1.0", "Betball", 5000);
+$(function() {
 
-const router = Router();
+  db.transaction(function(transaction) {
+    const sql = "CREATE TABLE User(" +
+      "username VARCHAR(50)," +
+      "password VARCHAR(50)," +
+      "email VARCHAR(50)," +
+      "phone VARCHAR(15)," +
+      "balance INTEGER," +
+      "PRIMARY KEY(username))";
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'betball',
-    password: 'pass',
-    database: 'Name'
-})
-
-router.use((req, res, next) => {
-    console.log('Request made to /USERS ROUTE');
-    next();
-});
-
-router.get('/', async (req, res) => {
-    //const results = await db.promise.query('SELECT * FROM USERS');
-    //console.log(results);
-    res.send(200);
-});
-
-router.get('/posts', (req, res) => {
-    res.json({ route: 'Posts' });
-});
-
-router.post('/', (req, res) => {
-    const { username, password } = req.body;
-    if (username && password) {
-        try {
-            //db.promise().query(`INSERT INTO USERS VALUES('${username}', '${password}')`);
-            //res.status(201).send({msg: 'Created User'});
-            document.getElementById('create-account').addEventListener(() => {
-                const username = document.getElementById('username').textContent;
-                const email = document.getElementById('email').textContent;
-                const pass = document.getElementById('pass').textContent;
-                const phone = document.getElementById('phone').textContent;
-                const balance = 100;
-            
-                try {
-                    db.promise().query(`INSERT INTO USER VALUES('${username}', '${pass}', '${email}', '${phone}', '${balance}')`);
-                } catch (err) {
-                    console.log(err);
-                }
-            });
-
-            document.getElementById('place').addEventListener(() => {
-
-                const bet_id = 0001;
-                const amount = document.getElementById('amount').textContent;
-                const description = 'Warriors vs. Heat';
-                const time = '7:30';
-                const payout = 2 * amount;
-                const username = 'TestUser';
-                const game_id = '0001';
-                const placed_odds = '+100';
-            
-                try {
-                    db.promise().query(`INSERT INTO BETS VALUES('${bet_id}', '${amount}', '${description}', '${time}', '${payout}', '${username}', '${game_id}', '${placed_odds}')`);
-                } catch (err) {
-                    console.log(err);
-                }
-            });
-
-
-        } catch (err) {
-            console.log(err);
-        }
-        
-    }
-});
-
-document.getElementById('login').addEventListener(() => {
+      transaction.executeSql(sql, undefined, function() {
+        alert("User table created sucessfully")
+      }, function() {
+        alert("User table is already created")
+      });
+  })
 
 });
 
+$("#create-account").click(function() {
+  const username = $("#username").val();
+  const email = $('#email').val();
+  const pass = $('#pass').val();
+  const phone = $('#phone').val();
+  const balance = 0;
 
+  db.transaction(function(transaction) {
 
+    const sql = `INSERT INTO USER VALUES('${username}', '${pass}', '${email}', '${phone}', '${balance}')`;
+    transaction.executeSql(sql, [username, pass, email, phone, balance], function(){
+      alert('New item is added successfully');
+    }, function(transaction, err) {
+      alert(err.message);
+    });
 
+  });
 
-
-
+});
