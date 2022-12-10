aa6793748b502c06e80eaeeff33213814569e710
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2022-12-10 16:54:11 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -22,47 +22,43 @@ $(function () {
 
         });
 
-        const odds = 'SELECT * FROM Odds';
-            transaction.executeSql(
-              odds,
-              undefined,
-              function (transaction, result) {
-                if (result.rows.length) {
-                  for (let i = 0; i < result.rows.length; i++) {
-                    const row = result.rows.item(i);
-                    const game_ID = row.game_ID;
-                    const team1 = row.team1;
-                    const team2 = row.team2;
-                    const team1_odds = row.team1_odds;
-                    const team2_odds = row.team2_odds;
-                    $('#odds-list').append(
-                      '<tr><td>' +
-                        game_ID +
-                        '</td><td>' +
-                        team1 +
-                        '</td><td>' +
-                        team2 +
-                        '</td><td>' +
-                        team1_odds +
-                        '</td><td>' +
-                        team2_odds +
-                        '</td></td>'
-                    );
-                  }
-                } else {
-                  $('#odds-list').append(
-                    '<tr><td colspan="6" align="center">No item found</td></tr>'
-                  );
-                }
-              },
-              function (transaction, err) {
-                alert(err.message);
-              }
-            );
-
-
-
-
+        // const odds = 'SELECT * FROM Odds';
+        //     transaction.executeSql(
+        //       odds,
+        //       undefined,
+        //       function (transaction, result) {
+        //         if (result.rows.length) {
+        //           for (let i = 0; i < result.rows.length; i++) {
+        //             const row = result.rows.item(i);
+        //             const game_ID = row.game_ID;
+        //             const team1 = row.team1;
+        //             const team2 = row.team2;
+        //             const team1_odds = row.team1_odds;
+        //             const team2_odds = row.team2_odds;
+        //             $('#odds-list').append(
+        //               '<tr><td>' +
+        //                 game_ID +
+        //                 '</td><td>' +
+        //                 team1 +
+        //                 '</td><td>' +
+        //                 team2 +
+        //                 '</td><td>' +
+        //                 team1_odds +
+        //                 '</td><td>' +
+        //                 team2_odds +
+        //                 '</td></td>'
+        //             );
+        //           }
+        //         } else {
+        //           $('#odds-list').append(
+        //             '<tr><td colspan="6" align="center">No item found</td></tr>'
+        //           );
+        //         }
+        //       },
+        //       function (transaction, err) {
+        //         alert(err.message);
+        //       }
+        //     );
 
       });
 });
